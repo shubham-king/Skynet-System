7ca2bd502d8cd9d60f709c84d0b78fc1066c820c
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-09-25 09:16:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import Skynet_System.config as Config
   # REQUIRED
     API_ID_KEY = "8222319"
     API_HASH_KEY = "ec0fa1dd5909e46538081fc0feb2ba35"  
-    STRING_SESSION = ""
+    STRING_SESSION = "1AZWarzgBu3WHFbi4ZFLg_NZcUPT-FU14CswCLABoB-RId1Wqt-e0J1NYzyOE9WqnTormwWLzGJrJML4OkFgkanIWQb_mcXo-M18v2wl-zYmsOUhSBo3YhWa7f9WXuY3u2iYNH3W2yM0u-qOBWd09qJ0k0JLxE8OqkFZyP4cYToybHTx9OpPNtCKdAoL-WN8ZpZzMmPhG3-afd_iQUFv_J2HchmywiOewztRLuOvGmCUZDd8oWU59rdtfTkY4GRvVut3OeNqOPgWvHos3IBj9CuFUsI9bPrqxui00P8e4V7ZNbcpzn-kXm-vNngwZZgh2FpmYMWHUvwHk7z2arEXwESLqRP1ObuQ="
     Skynet = ""
     ENFORCERS = ""
     Skynet_logs = "-1001589348091"
