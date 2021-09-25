a4f151e6879d1bd246d2ef16b90687cdc01b2fe0
shubham-king <84066395+shubham-king@users.noreply.github.com>
2021-09-25 09:07:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import Skynet_System.config as Config
 
 
   # REQUIRED
-    API_ID_KEY = "draj is op"
-    API_HASH_KEY = "lol dal isee "  
-    STRING_SESSION = "ssyour"
+    API_ID_KEY = "8222319"
+    API_HASH_KEY = "ec0fa1dd5909e46538081fc0feb2ba35"  
+    STRING_SESSION = ""
     Skynet = ""
     ENFORCERS = ""
-    Skynet_logs = ""
-    Skynet_approved_logs = ""
-    GBAN_MSG_LOGS = ""
+    Skynet_logs = "-1001589348091"
+    Skynet_approved_logs = "-1001589348091"
+    GBAN_MSG_LOGS = "-1001589348091"
